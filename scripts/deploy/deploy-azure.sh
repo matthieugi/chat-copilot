c1c02ab798325649e7cfe2d113f689e71a01e837
Matthieu Girard <matthieugirard@outlook.com>
2023-10-12 14:54:25 +0000

    add ai models to deployments scripts

--- scripts/deploy/deploy-azure.sh
+++ scripts/deploy/deploy-azure.sh
@@ -8,26 +8,29 @@ usage() {
     echo "Usage: $0 -d DEPLOYMENT_NAME -s SUBSCRIPTION -c BACKEND_CLIENT_ID -t AZURE_AD_TENANT_ID -ai AI_SERVICE_TYPE [OPTIONS]"
     echo ""
     echo "Arguments:"
-    echo "  -d, --deployment-name DEPLOYMENT_NAME      Name for the deployment (mandatory)"
-    echo "  -s, --subscription SUBSCRIPTION            Subscription to which to make the deployment (mandatory)"
-    echo "  -c, --client-id BACKEND_CLIENT_ID          Azure AD client ID for the Web API backend app registration (mandatory)"
-    echo "  -t, --tenant-id AZURE_AD_TENANT_ID         Azure AD tenant ID for authenticating users (mandatory)"
-    echo "  -ai, --ai-service AI_SERVICE_TYPE          Type of AI service to use (i.e., OpenAI or AzureOpenAI) (mandatory)"
-    echo "  -aiend, --ai-endpoint AI_ENDPOINT          Endpoint for existing Azure OpenAI resource"
-    echo "  -aikey, --ai-service-key AI_SERVICE_KEY    API key for existing Azure OpenAI resource or OpenAI account"
-    echo "  -rg, --resource-group RESOURCE_GROUP       Resource group to which to make the deployment (default: \"rg-\$DEPLOYMENT_NAME\")"
-    echo "  -r, --region REGION                        Region to which to make the deployment (default: \"South Central US\")"
-    echo "  -wr, --web-app-region WEB_APP_REGION       Region to deploy to the static web app into. This must be a region that supports static web apps. (default: \"West US 2\")"
-    echo "  -a, --app-service-sku WEB_APP_SVC_SKU      SKU for the Azure App Service plan (default: \"B1\")"
-    echo "  -i, --instance AZURE_AD_INSTANCE           Azure AD cloud instance for authenticating users"
-    echo "                                             (default: \"https://login.microsoftonline.com\")"
-    echo "  -ms, --memory-store                        Method to use to persist embeddings. Valid values are"
-    echo "                                             \"AzureCognitiveSearch\" (default), \"Qdrant\", \"Postgres\" and \"Volatile\""
-    echo "  -sap, --sql-admin-password                 Password for the PostgreSQL Server admin user"
-    echo "  -nc, --no-cosmos-db                        Don't deploy Cosmos DB for chat storage - Use volatile memory instead"
-    echo "  -ns, --no-speech-services                  Don't deploy Speech Services to enable speech as chat input"
-    echo "  -dd, --debug-deployment                    Switches on verbose template deployment output"
-    echo "  -ndp, --no-deploy-package                  Skips deploying the Web API package when set."
+    echo "  -d, --deployment-name DEPLOYMENT_NAME                   Name for the deployment (mandatory)"
+    echo "  -s, --subscription SUBSCRIPTION                         Subscription to which to make the deployment (mandatory)"
+    echo "  -c, --client-id BACKEND_CLIENT_ID                       Azure AD client ID for the Web API backend app registration (mandatory)"
+    echo "  -t, --tenant-id AZURE_AD_TENANT_ID                      Azure AD tenant ID for authenticating users (mandatory)"
+    echo "  -ai, --ai-service AI_SERVICE_TYPE                       Type of AI service to use (i.e., OpenAI or AzureOpenAI) (mandatory)"
+    echo "  -aiend, --ai-endpoint AI_ENDPOINT                       Endpoint for existing Azure OpenAI resource"
+    echo "  -aikey, --ai-service-key AI_SERVICE_KEY                 API key for existing Azure OpenAI resource or OpenAI account"
+    echo "  -planner, --ai-planner-model AI_PLANNER_MODEL           Planner model deployment name"
+    echo "  -completion, --ai-completion-model AI_COMPLETION_MODEL  Completion model deployment name"
+    echo "  -embedding, --ai-embedding-model AI_EMBEDDING_MODEL     Embedding model deployment name"
+    echo "  -rg, --resource-group RESOURCE_GROUP                    Resource group to which to make the deployment (default: \"rg-\$DEPLOYMENT_NAME\")"
+    echo "  -r, --region REGION                                     Region to which to make the deployment (default: \"South Central US\")"
+    echo "  -wr, --web-app-region WEB_APP_REGION                    Region to deploy to the static web app into. This must be a region that supports static web apps. (default: \"West US 2\")"
+    echo "  -a, --app-service-sku WEB_APP_SVC_SKU                   SKU for the Azure App Service plan (default: \"B1\")"
+    echo "  -i, --instance AZURE_AD_INSTANCE                        Azure AD cloud instance for authenticating users"
+    echo "                                                          (default: \"https://login.microsoftonline.com\")"
+    echo "  -ms, --memory-store                                     Method to use to persist embeddings. Valid values are"
+    echo "                                                          \"AzureCognitiveSearch\" (default), \"Qdrant\", \"Postgres\" and \"Volatile\""
+    echo "  -sap, --sql-admin-password                              Password for the PostgreSQL Server admin user"
+    echo "  -nc, --no-cosmos-db                                     Don't deploy Cosmos DB for chat storage - Use volatile memory instead"
+    echo "  -ns, --no-speech-services                               Don't deploy Speech Services to enable speech as chat input"
+    echo "  -dd, --debug-deployment                                 Switches on verbose template deployment output"
+    echo "  -ndp, --no-deploy-package                               Skips deploying the Web API package when set."
 }
 
 # Parse arguments
@@ -56,6 +59,21 @@ while [[ $# -gt 0 ]]; do
         ;;
         -ai|--ai-service)
         AI_SERVICE_TYPE="$2"
+        shift
+        shift
+        ;;
+        -planner|--ai-planner-model)
+        AI_PLANNER_MODEL="$2"
+        shift
+        shift
+        ;;
+        -completion|--ai-completion-model)
+        AI_COMPLETION_MODEL="$2"
+        shift
+        shift
+        ;;
+        -embedding|--ai-embedding-model)
+        AI_EMBEDDING_MODEL="$2"
         shift
         shift
         ;;
@@ -204,6 +222,9 @@ JSON_CONFIG=$(cat << EOF
     "webappLocation": { "value": "$WEB_APP_REGION" },
     "aiService": { "value": "$AI_SERVICE_TYPE" },
     "aiApiKey": { "value": "$AI_SERVICE_KEY" },
+    "plannerModel": { "value": "$AI_PLANNER_MODEL" },
+    "embeddingModel": { "value": "$AI_EMBEDDING_MODEL" },
+    "completionModel": { "value": "$AI_COMPLETION_MODEL" },
     "deployWebApiPackage": { "value": $([ "$NO_DEPLOY_PACKAGE" = true ] && echo "false" || echo "true") },
     "aiEndpoint": { "value": "$([ ! -z "$AI_ENDPOINT" ] && echo "$AI_ENDPOINT")" },
     "azureAdInstance": { "value": "$AZURE_AD_INSTANCE" },
